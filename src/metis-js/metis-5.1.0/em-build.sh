5d37480fa129cef09cbb09c2e108942b56d5f2a5
AIFanatic <aifanatic@github.com>
2024-03-24 05:18:27 -0100

    Add metis

---
+++ src/metis-js/metis-5.1.0/em-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+emcc -o metis.js -O3 -s WASM=1 \
+	--no-entry \
+	-s ALLOW_MEMORY_GROWTH=1 \
+	-s MODULARIZE=1 \
+	-s EXPORT_ES6=1 \
+	-s ENVIRONMENT=web \
+	-s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+	-s EXPORTED_FUNCTIONS='["_malloc"]' \
+    -DNDEBUG \
+	-I include \
+	-I GKlib \
+	-I libmetis \
+    wasm.c \
+    ./{GKlib,libmetis}/*.c
+
+cp ./metis.wasm /Users/mac/Downloads/temp/three-nanite/dist/
