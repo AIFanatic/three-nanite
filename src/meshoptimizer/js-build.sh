9bb2ffab3f1dcaab6b8efad83d8abdaf0ea3b712
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2024-04-07 15:02:17 +0000

    added _meshopt_simplifyScale

--- src/meshoptimizer/js-build.sh
+++ src/meshoptimizer/js-build.sh
@@ -9,7 +9,7 @@ emcc \
 -s ENVIRONMENT=web \
 -s ASSERTIONS=1 \
 -s EXPORTED_RUNTIME_METHODS="['cwrap', 'ccall']" \
--s EXPORTED_FUNCTIONS="['_malloc', '_meshopt_buildMeshletsBound', '_meshopt_buildMeshlets', '_meshopt_buildMeshletsScan', '_meshopt_simplify', '_meshopt_generateVertexRemap', '_meshopt_remapIndexBuffer', '_meshopt_remapVertexBuffer']" \
+-s EXPORTED_FUNCTIONS="['_malloc', '_meshopt_buildMeshletsBound', '_meshopt_buildMeshlets', '_meshopt_buildMeshletsScan', '_meshopt_simplify', '_meshopt_generateVertexRemap', '_meshopt_remapIndexBuffer', '_meshopt_remapVertexBuffer', '_meshopt_simplifyScale']" \
 ./src/clusterizer.cpp ./src/simplifier.cpp ./src/indexgenerator.cpp \
 -o ./js/meshoptimizer.js
 
